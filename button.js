5c8c99a5909c50bfef1a955fb463cb44fba3f5ed
MmCrunchyLotion <petersondane77@gmail.com>
2025-09-30 16:25:06 -0600

    Missed commits from in class: includes initial html page additions, some js that is irrelevant, and an icon

---
+++ button.js
@@ -0,0 +1,11 @@
+function showAlert1() {
+    // alert("You clicked me!");
+    document.getElementById("output").innerText = "You clicked me!";
+}
+function showAlert2() {
+    // alert("NOOOOOOOOOOOOOOOOOOOO!!!");
+    // document.getElementById("output").innerText = "NOOOOOOOOOOOOOOOOOOOO!!!";
+    // document.getElementById("output").style.color = "red";
+    // document.getElementById("output").style.fontWeight = "bold";
+    document.getElementById("output").innerHTML = "<img src = 'https://image-cdn.neatoshop.com/styleimg/62963/none/black/default/362104-20;1503657064y.jpg'>";
+}
